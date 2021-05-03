945f877f82631ebea299cccc0c70fa49a16fedcc
Dominik <cogentdom@gmail.com>
2021-05-03 15:29:04 -0600

    ...

---
+++ SurveyDataCollector/StarVC.swift
@@ -0,0 +1,22 @@
+//
+//  StarVC.swift
+//  SurveyDataCollector
+//
+//  Created by Dominik Huffield on 5/3/21.
+//
+
+import UIKit
+
+class StarVC: UIViewController {
+    
+    @IBOutlet weak var stars: CosmosView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        var x = stars.rating
+//        stars.didFinishTouchingCosmos
+    }
+    
+
+
+}
